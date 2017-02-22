fe6ad72b7aface029082a9c0625bdbc41eadd214
Ian <ianjblanchette@gmail.com>
2017-02-22 13:24:28 -0500

    Controller functional

--- src/car/CarController.java
+++ src/car/CarController.java
@@ -9,7 +9,8 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.DatePicker;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 /**
@@ -25,13 +26,37 @@ public class CarController implements Initializable {
 @FXML private TextField sellingPriceTextField;
 @FXML private TextField featuresListTextField;
         
-        
+@FXML private Button createCarButton;  
+@FXML private Label errorMessageLabel;
+
+private void createCarButtonPushed()
+{
+    try 
+    {
+        Car car1 = new Car(this.makeTextField.getText(),this.modelTextField.getText(), 
+                Integer.parseInt(this.yearTextField.getText()),
+                Double.parseDouble(this.sellingPriceTextField.getText()), 
+                this.featuresListTextField.getText()); 
+    System.out.printf("Car Created: %s %s%n", car1, car1.getClass());
+    }
+    catch (IllegalArguementException e)
+    {
+        this.errorMessageLabel.setText(e.getMessage());
+        this.errorMessageLabel.setVisible(true);
+    }
+}
+
+
+
+
     /**
      * Initializes the controller class.
      */
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+    public void initialize(URL url, ResourceBundle rb) 
+    {
+      this.errorMessageLabel.setVisible(false);  
+        
     }    
     
 }
